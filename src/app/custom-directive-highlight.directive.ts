e8436ea24fcba9b9b5e9085e530ae7ed765e3cef
MaurizioBrioschi <maurizio.brioschi@gmail.com>
2017-02-16 21:07:05 +0100

    make custom directive configurable

--- src/app/custom-directive-highlight.directive.ts
+++ src/app/custom-directive-highlight.directive.ts
@@ -1,23 +1,26 @@
-import { Directive, ElementRef, Renderer,HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer,HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCustomDirectiveHighlight]'
 })
 export class CustomDirectiveHighlightDirective {
   @HostListener('mouseenter') mouseover(){
-    this.backgroundColor = 'red';
+    this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseleave') mouseleave(){
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.defaultColor;
   }
   @HostBinding('style.backgroundColor') get setColor(){
     return this.backgroundColor;
   }
+  @Input() defaultColor = 'white';
+  @Input('highlight') highlightColor = 'green';
   private backgroundColor;
+
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
     //this.elementRef.nativeElement.style.backgroundColor = 'green';
-    this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color','green');
-    this.backgroundColor = 'green';
+    this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color',this.defaultColor);
+    this.backgroundColor = this.defaultColor;
   }
 
 }
