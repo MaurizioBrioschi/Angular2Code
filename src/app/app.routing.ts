ba4a55f85fd62b4d4c59b9730e2f969634ed5f3f
MaurizioBrioschi <brioschi@mothership.de>
2017-03-01 16:50:00 +0100

    set databinding route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import {Routes, RouterModule} from "@angular/router";
 import {PropertydatabinbdingComponent} from "./databinding/propertydatabinbding.component";
 import {LifeCycleComponent} from "./life-cycle/life-cycle.component";
+import {AppComponent} from "./app.component";
 
 const APP_ROUTES: Routes = [
     {path: 'databinding', component: PropertydatabinbdingComponent }
